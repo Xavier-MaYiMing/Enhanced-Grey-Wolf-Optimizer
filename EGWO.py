1f1d98e1ace2ae906d4aa2892371db5a37dbd54f
Yi-Ming Ma (Xavier Ma) <108516554+Xavier-MaYiMing@users.noreply.github.com>
2022-11-22 13:25:18 +0800

    Add files via upload

---
+++ EGWO.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/21 15:09
+# @Author  : Xavier Ma
+# @Email   : xavier_mayiming@163.com
+# @File    : EGWO.py
+# @Statement : Enhanced Grey Wolf Optimizer
+# @Reference : Kaiping Luo. Enhanced grey wolf optimizer with a model for dynamically estimating the location of the prey. Applied Soft Computing. 2019,77 :225-235.
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+# def obj(x):
+#     """
+#     Shifted Rastrigin function
+#     # Dimension: 30
+#     # Range: [-4.12, 6.12]
+#     """
+#     num = 0
+#     for i in range(len(x)):
+#         if not -4.12 <= x[i] <= 6.12:
+#             return 1e6
+#         num += (x[i] - 1) ** 2 - 10 * math.cos(2 * math.pi * (x[i] - 1)) + 10
+#     return num
+
+
+def obj(x):
+    """
+    The objective function of pressure vessel design
+    :param x:
+    :return:
+    """
+    x1 = x[0]
+    x2 = x[1]
+    x3 = x[2]
+    x4 = x[3]
+    g1 = -x1 + 0.0193 * x3
+    g2 = -x2 + 0.00954 * x3
+    g3 = -math.pi * x3 ** 2 - 4 * math.pi * x3 ** 3 / 3 + 1296000
+    g4 = x4 - 240
+    if g1 <= 0 and g2 <= 0 and g3 <= 0 and g4 <= 0:
+        return 0.6224 * x1 * x3 * x4 + 1.7781 * x2 * x3 ** 2 + 3.1661 * x1 ** 2 * x4 + 19.84 * x1 ** 2 * x3
+    else:
+        return 1e10
+
+
+def main(pop, iter, lb, ub):
+    """
+    The main function of EGWO
+    :param pop: the number of wolves
+    :param iter: the iteration number
+    :param lb: the lower bound (list)
+    :param ub: the upper bound (list)
+    :return:
+    """
+    # Step 1. Initialization
+    dim = len(lb)  # dimension
+    pos = []  # the position of wolves
+    score = []  # the score of wolves
+    iter_best = []  # the best-so-far score of each iteration
+    for _ in range(pop):
+        temp_pos = [random.uniform(lb[i], ub[i]) for i in range(dim)]
+        pos.append(temp_pos)
+        score.append(obj(temp_pos))
+    sorted_score = sorted(score)
+    alpha_score = sorted_score[0]  # the score of the alpha wolf
+    beta_score = sorted_score[1]  # the score of the beta wolf
+    delta_score = sorted_score[2]  # the score of the delta wolf
+    alpha_pos = pos[score.index(alpha_score)].copy()  # the position of the alpha wolf
+    beta_pos = pos[score.index(beta_score)].copy()  # the position of the beta wolf
+    delta_pos = pos[score.index(delta_score)].copy()  # the position of the delta wolf
+    gbest = alpha_score  # the global best score
+    gbest_pos = alpha_pos.copy()  # the global best position
+    con_iter = 0
+
+    # Step 2. The main loop
+    for t in range(iter):
+
+        # Step 2.1. Update the prey
+        temp_weight = [random.uniform(1, 3) for _ in range(3)]
+        temp_weight = sorted(temp_weight, reverse=True)
+        sum_weight = sum(temp_weight)
+        omega = [temp_weight[i] / sum_weight for i in range(3)]
+        std = math.exp(-100 * (t + 1) / iter)  # the standard deviation of the simulated stochastic error
+        prey_pos = [omega[0] * alpha_pos[i] + omega[1] * beta_pos[i] + omega[2] * delta_pos[i] + random.normalvariate(0, std) for i in range(dim)] 
+
+        # Step 2.2. Update the position of all wolves
+        for i in range(pop):
+            for j in range(dim):
+                y = prey_pos[j] - random.uniform(-2, 2) * abs(prey_pos[j] - pos[i][j])
+                if y > ub[j]:
+                    pos[i][j] = pos[i][j] + random.random() * (ub[j] - pos[i][j])
+                elif y < lb[j]:
+                    pos[i][j] = pos[i][j] - random.random() * (lb[j] - pos[i][j])
+                else:
+                    pos[i][j] = y
+            
+            # Step 2.3. Update alpha, beta, and delta wolves
+            score[i] = obj(pos[i])
+            if score[i] < alpha_score:
+                alpha_score = score[i]
+                alpha_pos = pos[i].copy()
+            elif score[i] < beta_score:
+                beta_score = score[i]
+                beta_pos = pos[i].copy()
+            elif score[i] < delta_score:
+                delta_score = score[i]
+                delta_pos = pos[i].copy()
+
+            # Step 2.4. Update the global best
+            if score[i] < gbest:
+                gbest = score[i]
+                gbest_pos = pos[i].copy()
+                con_iter = t + 1
+        iter_best.append(gbest)
+
+    # Step 3. Sort the results
+    x = [i for i in range(iter)]
+    plt.figure()
+    plt.plot(x, iter_best, linewidth=2, color='blue')
+    plt.xlabel('Iteration number')
+    plt.ylabel('Global optimal value')
+    plt.title('Convergence curve')
+    plt.show()
+    return {'best score': gbest, 'best solution': gbest_pos, 'convergence iteration': con_iter}
+
+
+if __name__ == '__main__':
+    # Parameter settings
+    pop = 50
+    iter = 2000
+    lb = [0, 0, 10, 10]
+    ub = [99, 99, 200, 200]
+    print(main(pop, iter, lb, ub))
---
+++ README.md
@@ -0,0 +1,96 @@
+### Enhanced Grey Wolf Optimizer
+
+##### Reference: Kaiping Luo. Enhanced grey wolf optimizer with a model for dynamically estimating the location of the prey. Applied Soft Computing. 2019,77 :225-235.
+
+| Variables   | Meaning                                                |
+| ----------- | ------------------------------------------------------ |
+| pop         | The number of wolves                                   |
+| iter        | The iteration number                                   |
+| lb          | The lower bound (list)                                 |
+| ub          | The upper bound (list)                                 |
+| pos         | The set of wolves (list)                               |
+| score       | The score of wolves (list)                             |
+| dim         | Dimension (list)                                       |
+| alpha_score | The score of the alpha wolf                            |
+| alpha_pos   | The position of the alpha wolf (list)                  |
+| beta_score  | The score of the beta wolf                             |
+| beta_pos    | The position of the beta wolf (list)                   |
+| delta_score | The score of the delta wolf                            |
+| delta_pos   | The position of the delta wolf (list)                  |
+| prey_pos    | The dynamically estimating position of the prey (list) |
+| gbest       | The score of the global best score                     |
+| gbest_pos   | The position of the global best (list)                 |
+| iter_best   | The global best score of each iteration (list)         |
+| con_iter    | The last iteration number when "gbest" is updated      |
+
+#### Test problem: Pressure vessel design
+
+![](C:\Users\dell\Desktop\研究生\个人算法主页\Grey Wolf Optimizer\Pressure vessel design.png)
+$$
+\text{min}\ f(x)=0.6224x_1x_3x_4+1.7781x_2x_3^2+3.1661x_1^2x_4+19.84x_1^2x_3,\\
+\text{s.t.} -x_1+0.0193x_3\leq0,\\
+-x_3+0.0095x_3\leq0,\\
+-\pi x_3^2x_4-\frac{4}{3}\pi x_3^3+1296000\leq0,\\
+x_4-240\leq0,\\
+0\leq x_1\leq99,\\
+0\leq x_2 \leq99,\\
+10\leq x_3 \leq 200,\\
+10\leq x_4 \leq 200.
+$$
+
+
+#### Example
+
+```python
+if __name__ == '__main__':
+    # Parameter settings
+    pop = 50
+    iter = 2000
+    lb = [0, 0, 10, 10]
+    ub = [99, 99, 200, 200]
+    print(main(pop, iter, lb, ub))
+```
+
+##### Output:
+
+![convergence curve](C:\Users\dell\Desktop\研究生\个人算法主页\Enhanced Grey Wolf Optimizer\convergence curve.png)
+
+The EGWO converges at its 1,271-th iteration, and the global best value is 8050.913534658795. 
+
+```python
+{
+    'best score': 8050.913534658795, 
+    'best solution': [1.3005502034963052, 0.6428626394484327, 67.3860209065443, 10.0], 
+    'convergence iteration': 1271
+}
+```
+
+#### Compared with the GWO on the shifted functions
+
+##### Shifted sphere function
+
+$$
+f(x)=\sum_{i=1}^{30}(x_i-0.0001)^2,\qquad x_i\in[-10, 100], \quad i=1,\cdots,30.
+$$
+
+![Shifted sphere function](C:\Users\dell\Desktop\研究生\个人算法主页\Enhanced Grey Wolf Optimizer\Shifted sphere function.png)
+
+###### The best value found by the GWO is 1.000035678101285e-08.
+
+###### The best value found by the EGWO is 2.115383003666077e-25.
+
+##### Shifted Rastrigin function
+
+$$
+f(x)=\sum_{i=1}^{30}((x_i-1)^2-10\cos(2\pi(x_i-1)) + 10),\qquad x_i\in[-4.12, 5.12], \quad i=1,\cdots,30.
+$$
+
+![Shifted Rastrigin function](C:\Users\dell\Desktop\研究生\个人算法主页\Enhanced Grey Wolf Optimizer\Shifted Rastrigin function.png)
+
+###### The best value found by the GWO is 25.32141353244897.
+
+###### The best value found by the EGWO is 8.954631513839622.
+
+##### Conclusion
+
+If the global optimal solution shifts away from the origin of the coordination, the performance of the original GWO deteriorates sharply. The EGWO solves this problem, and its performance on shifted functions proves its effectiveness.
